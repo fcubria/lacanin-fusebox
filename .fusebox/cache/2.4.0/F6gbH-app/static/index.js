a68c17cf5251052f2537ccbdc3ec120915277859
fcubria <fcubria@gmail.com>
2017-10-28 13:44:34 +0200

    borrado .fusebox

--- .fusebox/cache/2.4.0/F6gbH-app/static/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = { contents: "\"use strict\";\r\nexports.__esModule = true;\r\nrequire(\"./scss/main.scss\");\r\ndocument.body.innerHTML = \"\\n    <div class=\\\"content\\\">\\n    <h1>Welcome to FuseBox!</h1>\\n    </div>\";\r\n",
-dependencies: ["./scss/main.scss"],
-sourceMap: {},
-headerContent: undefined,
-mtime: 1509188863529,
-devLibsRequired : undefined,
-_ : {}
-}
